99e53d1e802590c1b403d5ec799d925c998563fc
vpatel07 <vpatel077375@gmail.com>
2023-04-23 00:28:35 -0400

    finisehd part 1-3

---
+++ var_and_data_types/bin/example.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  const randomThing =
+      "foo"; /* const -> value can't change from the value that is assigned (constant) and doesn't allow re-assignment. Is a kind of variable, immutable */
+
+  print(randomThing);
+
+  final age =
+      200; /* final -> you can can the value that is assigned but you cannot re-assign. Different kind of immunibility */
+  print(age);
+}
